286859792d3bcd8234d7078f3f58188c97e3c359
ssaegert <ssaegert@gmail.com>
2018-01-26 15:30:07 -0600

    Media query exercise

---
+++ js/external.js
@@ -0,0 +1,6 @@
+"use strict";
+console.log("Hello from external JavaScript");
+alert("Welcome to my website!");
+var favoriteColor = prompt("What is your favorite color?");
+var mineToo = "Great," + " " + favoriteColor + " " + "is my favorite color too!";
+alert (mineToo);
